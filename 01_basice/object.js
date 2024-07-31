46f7ca14ada7e26e596aab6cd3c142b3fb896647
Simteyaj <153082697+Simteyaj@users.noreply.github.com>
2024-07-31 11:58:05 +0000

    objectjs

---
+++ 01_basice/object.js
@@ -0,0 +1,8 @@
+const course={
+    coursename:"js in hindi laguage",
+    price:"999",
+    courseInstructor:"imteyaj"
+}
+//course.courseInstructor
+const{courseInstructor}=course
+console.log(courseInstructor);
